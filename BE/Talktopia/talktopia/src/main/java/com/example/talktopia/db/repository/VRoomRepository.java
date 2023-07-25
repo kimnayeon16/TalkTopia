7f8dd6bc03dfdd033f8ec4f4a3a382c55666b4a1
wodnr8462 <wodnr8462@naver.com>
2023-07-25 13:22:04 +0900

    feat: 랜덤Controller추가

--- BE/Talktopia/talktopia/src/main/java/com/example/talktopia/db/repository/VRoomRepository.java
+++ BE/Talktopia/talktopia/src/main/java/com/example/talktopia/db/repository/VRoomRepository.java
@@ -1,4 +1,17 @@
 package com.example.talktopia.db.repository;
 
-public interface VRoomRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.talktopia.db.entity.vr.VRoom;
+
+public interface VRoomRepository extends JpaRepository<VRoom, Long> {
+
+	@Query("SELECT vrSession FROM VRoom")
+	List<String> findAllIds();
+
+	VRoom findByVrSession(String roomId);
 }
+
