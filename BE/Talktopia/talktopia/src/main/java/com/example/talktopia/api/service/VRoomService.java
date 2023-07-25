6216b476d1ab4ec2612879e82d3d6c717cb49c09
wodnr8462 <wodnr8462@naver.com>
2023-07-25 10:38:33 +0900

    feat: 랜덤방기능구현

---
+++ BE/Talktopia/talktopia/src/main/java/com/example/talktopia/api/service/VRoomService.java
@@ -0,0 +1,68 @@
+package com.example.talktopia.api.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.talktopia.api.request.VRoom.VRoomReq;
+import com.example.talktopia.api.response.VRoom.VRoomRes;
+import com.example.talktopia.db.entity.User;
+import com.example.talktopia.db.entity.vr.VRoom;
+import com.example.talktopia.db.repository.UserRepository;
+import com.example.talktopia.db.repository.VRoomRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class VRoomService {
+
+	private final VRoomRepository vroomrepsitory;
+	private final UserRepository userRepository;
+	private final ParticipantsService participantsService;
+
+	public List<String> quickRoom() {
+		return vroomrepsitory.findAllIds();
+	}
+
+	public VRoomRes getRoomRes(String minConnRoomId, String userId) throws Exception {
+
+		VRoom vRoom1 = vroomrepsitory.findByVrSession(minConnRoomId);
+
+		vRoom1.setVrCurrCnt(vRoom1.getVrCurrCnt() + 1);
+		if (vRoom1.getVrCurrCnt() == vRoom1.getVrMaxCnt()) {
+			vRoom1.setVrEnter(false);
+		}
+
+		User user = userRepository.findByUserId(userId).orElseThrow(() -> new Exception("유저가없엉 ㅋ"));
+		participantsService.joinRoom(user, vRoom1);
+
+		VRoomRes vRoom = new VRoomRes();
+		vRoom.setVr_session(minConnRoomId);
+		vRoom.setVr_enter(true);
+		vRoom.setVr_create_time(LocalDateTime.now());
+
+		return vRoom;
+	}
+
+	@Transactional
+	public void makeRoom(String roomId, final VRoomReq VRoomReq) throws Exception {
+		log.info(VRoomReq.getUserId());
+		User user = userRepository.findByUserId(VRoomReq.getUserId()).orElseThrow(() -> new Exception("유저가없엉"));
+		VRoom room = VRoom.builder()
+			.vrSession(roomId)
+			.vrCreateTime(LocalDateTime.now())
+			.vrMaxCnt(VRoomReq.getVr_max_cnt())
+			.vrEnter(true)
+			.build();
+
+		vroomrepsitory.save(room);
+
+		participantsService.joinRoom(user, room);
+	}
+}
---
+++ BE/Talktopia/talktopia/src/main/java/com/example/talktopia/db/entity/vr/Participants.java
@@ -0,0 +1,51 @@
+package com.example.talktopia.db.entity.vr;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.example.talktopia.db.entity.User;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Participants {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long partId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "vroom_id")
+	private VRoom vRoom;
+
+	@Builder
+	public Participants(long partId, User user, VRoom vRoom) {
+		this.partId = partId;
+		setvRoom(vRoom);
+		setUser(user);
+	}
+
+	private void setvRoom(VRoom vRoom) {
+		this.vRoom = vRoom;
+		vRoom.getParticipantsList().add(this);
+	}
+
+	private void setUser(User user) {
+		this.user = user;
+		user.getParticipantsList().add(this);
+	}
+
+}
