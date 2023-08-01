f56763aff02163a01e7921d34bab05ec8abb71b7
wodnr8462 <wodnr8462@naver.com>
2023-08-01 14:15:53 +0900

    hotfix: vrservice,vrcontroller change

--- BE/Talktopia/talktopia/src/main/java/com/example/talktopia/api/service/VRoomService.java
+++ BE/Talktopia/talktopia/src/main/java/com/example/talktopia/api/service/VRoomService.java
@@ -3,6 +3,7 @@ package com.example.talktopia.api.service;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.example.talktopia.api.request.VRoomReq;
