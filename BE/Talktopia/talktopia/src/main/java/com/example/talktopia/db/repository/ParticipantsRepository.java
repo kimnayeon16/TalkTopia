6726154046246ba5358218e801ebe1d21155a2e5
SuIn Woo <wbo1026@naver.com>
2023-08-03 12:02:38 +0900

    hotfix: Transactional 추가

--- BE/Talktopia/talktopia/src/main/java/com/example/talktopia/db/repository/ParticipantsRepository.java
+++ BE/Talktopia/talktopia/src/main/java/com/example/talktopia/db/repository/ParticipantsRepository.java
@@ -2,12 +2,16 @@ package com.example.talktopia.db.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.talktopia.db.entity.vr.Participants;
 
+@Repository
 public interface ParticipantsRepository extends JpaRepository<Participants, Long> {
 
 	boolean existsByUser_UserNo(long userNo);
 
+	@Transactional
 	void deleteByUser_UserNo(long userNo);
 }
