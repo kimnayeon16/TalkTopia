2e78bb7a16ba8f4dd05f1a2177102d9089213733
김준석 <pepaa15@naver.com>
2023-08-03 17:46:47 +0900

    feat: 화상채팅 페이지 번역 api 적용

--- FE/talktopia/src/utils/env.js
+++ FE/talktopia/src/utils/env.js
@@ -1,3 +1,2 @@
 export const BACKEND_URL = "https://talktopia.site:10001";
-export const REACT_APP_X_RAPID_API_KEY= "789f74e95cmsh021fc65d6b5b7d5p15af28jsn304a9406d5a4";
-
+export const REACT_APP_X_RAPID_API_KEY= "87b4571f9emsh1fddaf42f19a0d9p13d9a2jsnd011f199f37f";
