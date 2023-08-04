7f4935ef98430254c6f0dfe7289b8a4ddbd7c1a6
malpipapi <wodnr8462@naver.com>
2023-08-04 21:14:22 +0900

    hotfix: port변경

--- FE/talktopia/src/utils/env.js
+++ FE/talktopia/src/utils/env.js
@@ -1,3 +1,2 @@
-// export const BACKEND_URL = "https://talktopia.site:10001";
-export const BACKEND_URL = "https://localhost:8000";
+export const BACKEND_URL = "https://talktopia.site:10001";
 export const REACT_APP_X_RAPID_API_KEY= "a63d6410c3msh3cee8298116ba4ep107d84jsnbe023f5ac5ca";
