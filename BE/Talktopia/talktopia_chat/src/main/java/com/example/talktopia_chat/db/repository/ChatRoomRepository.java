b75d05e73bf88bd85dd0a6ad65fd79b533f48954
이진서 <lalaququ@naver.com>
2023-08-09 11:24:26 +0900

    fix: Write Back 스케쥴러 수정 및 1분간 실행

--- BE/Talktopia/talktopia_chat/src/main/java/com/example/talktopia_chat/db/repository/ChatRoomRepository.java
+++ BE/Talktopia/talktopia_chat/src/main/java/com/example/talktopia_chat/db/repository/ChatRoomRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.talktopia_chat.db.entity.ChatRoom;
 import com.example.talktopia_chat.db.entity.ChatRoomParticipants;
@@ -15,12 +16,6 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 	Optional<ChatRoom> findByCrSession(String crSession);
 
 	List<ChatRoom> findAll();
-
-	// // ChatRoom 엔티티의 id를 통해 SaveChatRoomContent 엔티티 조회
-	// SaveChatRoomContentRepository findSaveChatRoomContentByCrNo(long crNo);
-	//
-	// // ChatRoom 엔티티의 id를 통해 ChatRoomParticipants 엔티티 조회
-	// ChatRoomParticipants findChatRoomParticipantsByCrno(long crNo);
 
 	// 외부채팅방 삭제
 	Optional<ChatRoom> deleteByCrNo(long crNo);
