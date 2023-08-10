1eb2fc7e0cf1c4d5de955e3a055684258a37aeb5
김준석 <pepaa15@naver.com>
2023-08-10 10:07:34 +0900

    feat: 음소거 토글 시 음성인식 토글되는 기능 추가

--- FE/talktopia/src/components/video/ConversationLog.js
+++ FE/talktopia/src/components/video/ConversationLog.js
@@ -14,73 +14,19 @@ function ConversationLog(props) {
     // WebSpeechApi관련 state
     const [transcript, setTranscript] = useState(""); // 자신이 말한 텍스트
 
-    const recognition = new window.webkitSpeechRecognition();
-    recognition.continuous = true;
-    recognition.lang = user.sttLang;    // 음성 인식되는 언어
-    recognition.interimResults = true;
-    recognition.maxAlternatives = 1;
 
-    // speech recognition 이벤트핸들러 시작
-    recognition.onspeechend = function () {
-        recognition.stop();
-        console.log("speech end")
+    // speech recognition toggle
+    const recognitionEnableRef = useRef(false)
+    const [recognition, setRecognition] = useState(null);
+
+    if (!!props.isAudioActive && !recognitionEnableRef.current && recognition !== null) {
+        recognitionEnableRef.current = true
+        recognition.start();
+    } else if (!props.isAudioActive && recognitionEnableRef.current && recognition !== null) {
+        recognitionEnableRef.current = false
+        recognition.abort();
     }
 
-    recognition.onerror = function (event) {
-        console.log('Error occurred in recognition: ' + event.error);
-    }
-
-    recognition.onaudiostart = function (event) {
-        //Fired when the user agent has started to capture audio.
-        console.log('onaudiostart');
-    }
-
-    recognition.onaudioend = function (event) {
-        //Fired when the user agent has finished capturing audio.
-        console.log('onaudioend');
-    }
-
-    recognition.onend = function (event) {
-        //Fired when the speech recognition service has disconnected.
-        console.log('한 뭉탱이 인식이 끝났습니다.');
-    }
-
-    recognition.onnomatch = function (event) {
-        //Fired when the speech recognition service returns a final result with no significant recognition. This may involve some degree of recognition, which doesn't meet or exceed the confidence threshold.
-        console.log('nomatch');
-    }
-
-    recognition.onsoundstart = function (event) {
-        //Fired when any sound — recognisable speech or not — has been detected.
-        console.log('on sound start');
-    }
-
-    recognition.onsoundend = function (event) {
-        //Fired when any sound — recognisable speech or not — has stopped being detected.
-        console.log('on sound end');
-    }
-
-    recognition.onspeechstart = function (event) {
-        //Fired when sound that is recognised by the speech recognition service as speech has been detected.
-        console.log('onspeechstart');
-    }
-    recognition.onstart = function (event) {
-        //Fired when the speech recognition service has begun listening to incoming audio with intent to recognize grammars associated with the current SpeechRecognition.
-        console.log('onstart');
-    }
-
-    // 음성 인식 결과 반환 시
-    recognition.onresult = (e) => {
-        // e.results 배열의 마지막 인덱스를 가져와서 처리합니다.
-        const lastResult = e.results[e.results.length - 1];
-        setTranscript(lastResult[0].transcript);  // 내가 말한 내용
-
-        // isFinal 프로퍼티를 통해 해당 결과가 최종 결과인지 확인합니다.
-        if (lastResult.isFinal) {                 // true이면 말 다했기에 전달함수 호출
-            sendMessage(lastResult[0].transcript);  // 메세지 전달 함수 호출
-        }
-    };
-    // speech recognition 이벤트핸들러 끝
 
     // 메세지 전달 함수
     const sendMessage = (finalTranscript) => {
@@ -108,7 +54,84 @@ function ConversationLog(props) {
     };
 
     useEffect(() => {
-        recognition.start();    // 마운트 시 바로 음성 인식 되게
+        // speech recognition 설정
+        const recognitionInstance = new window.webkitSpeechRecognition();
+        recognitionInstance.continuous = true;
+        recognitionInstance.lang = user.sttLang;    // 음성 인식되는 언어
+        recognitionInstance.interimResults = true;
+        recognitionInstance.maxAlternatives = 1;
+    
+        // speech recognition 이벤트핸들러 시작
+        recognitionInstance.onspeechend = function () {
+            recognitionInstance.stop();
+            console.log("speech end")
+        }
+    
+        recognitionInstance.onerror = function (event) {
+            console.log('Error occurred in recognition: ' + event.error);
+        }
+    
+        recognitionInstance.onaudiostart = function (event) {
+            //Fired when the user agent has started to capture audio.
+            console.log('onaudiostart');
+        }
+    
+        recognitionInstance.onaudioend = function (event) {
+            //Fired when the user agent has finished capturing audio.
+            console.log('onaudioend');
+        }
+    
+        recognitionInstance.onend = function (event) {
+            //Fired when the speech recognition service has disconnected.
+            if (recognitionEnableRef.current) {
+                recognitionInstance.start();
+                console.log('재연결 되었습니다.')
+            } else {
+                console.log('한 뭉탱이 인식이 끝났습니다.');
+            }
+        }
+    
+        recognitionInstance.onnomatch = function (event) {
+            //Fired when the speech recognition service returns a final result with no significant recognition. This may involve some degree of recognition, which doesn't meet or exceed the confidence threshold.
+            console.log('nomatch');
+        }
+    
+        recognitionInstance.onsoundstart = function (event) {
+            //Fired when any sound — recognisable speech or not — has been detected.
+            console.log('on sound start');
+        }
+    
+        recognitionInstance.onsoundend = function (event) {
+            //Fired when any sound — recognisable speech or not — has stopped being detected.
+            console.log('on sound end');
+        }
+    
+        recognitionInstance.onspeechstart = function (event) {
+            //Fired when sound that is recognised by the speech recognition service as speech has been detected.
+            console.log('onspeechstart');
+        }
+        recognitionInstance.onstart = function (event) {
+            //Fired when the speech recognition service has begun listening to incoming audio with intent to recognize grammars associated with the current SpeechRecognition.
+            console.log('onstart');
+        }
+    
+        // 음성 인식 결과 반환 시
+        recognitionInstance.onresult = (e) => {
+            // e.results 배열의 마지막 인덱스를 가져와서 처리합니다.
+            const lastResult = e.results[e.results.length - 1];
+            setTranscript(lastResult[0].transcript);  // 내가 말한 내용
+    
+            // isFinal 프로퍼티를 통해 해당 결과가 최종 결과인지 확인합니다.
+            if (lastResult.isFinal) {                 // true이면 말 다했기에 전달함수 호출
+                sendMessage(lastResult[0].transcript);  // 메세지 전달 함수 호출
+            }
+        };
+        // speech recognition 이벤트핸들러 끝
+        setRecognition(recognitionInstance)
+
+        recognitionInstance.start();        // 마운트 시 바로 음성 인식 되게
+        recognitionEnableRef.current = true // 음성 인식 활성화 확인
+
         // Receiver of the message (usually before calling 'session.connect')
         props.mainStreamManager.stream.session.on('signal:STT', async (event) => {
             const data = JSON.parse(event.data);
@@ -133,7 +156,10 @@ function ConversationLog(props) {
         });
 
         return () => {
-            recognition.abort();
+            if (recognitionEnableRef.current) {
+                recognitionEnableRef.current = false
+                recognitionInstance.abort();
+            }
         };
     }, []);
 
@@ -209,43 +235,6 @@ function ConversationLog(props) {
  
         </>
     );
-
-    // return (
-    //     <>
-    //         <div id={`${ style.chatContainer }`}>
-    //             <div id={`${ style.chatComponent }`}>
-    //                 <div id={`${ style.chatToolbar }`}>
-    //                     <span>Conversation</span>
-    //                 </div>
-    
-    //                 <div className={`${ style.message_wrap }`} ref={chatScroll}>
-    //                     {messageList.map((data, i) => (
-    //                         <div 
-    //                             key={`${i}-Conversation`}
-    //                             className={
-    //                                 `${style.message} ${ data.connectionId !== props.mainStreamManager.session.connection.connectionId ? style.left : style.right }`
-    //                             }
-    //                         >
-    //                             <div className={ `${style.msg_detail }`}>
-    //                                 <div className={ `${style.msg_info }`}>
-    //                                     <p>{data.nickname}</p>
-    //                                 </div>
-    //                                 <div className={ `${style.msg_content }`}>
-    //                                     <p className={ `${style.text }`}>{data.transcript} ( {data.translate} )</p>
-    //                                 </div>
-    //                             </div>
-    //                         </div>
-    //                     ))}
-    //                 </div>  
-
-
-    //                 <div id={ `${style.transcriptInput }`}>
-    //                     <p>{transcript}</p>
-    //                 </div>
-    //             </div>
-    //         </div>
-    //     </>
-    // )
 };
 
 export default ConversationLog;
