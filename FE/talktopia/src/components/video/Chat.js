4d0f377c16b3103084eda85e83d7a9cf6715a5c6
김준석 <pepaa15@naver.com>
2023-08-13 02:01:23 +0900

    fix: papago api로 변경

--- FE/talktopia/src/components/video/Chat.js
+++ FE/talktopia/src/components/video/Chat.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import axios from "axios";
+import { BACKEND_URL } from '../../utils';
 
-import { REACT_APP_X_RAPID_API_KEY } from "../../utils";
+
+// import { REACT_APP_X_RAPID_API_KEY } from "../../utils";
 import style from './Chat.module.css';
 
 function Chat(props) {
@@ -76,34 +78,62 @@ function Chat(props) {
         setMessage('');
     };
 
-    // 번역 함수
-    const translationHandler = async (text, sourceLanguage, targetLanguage) => {
-        const encodedParams = new URLSearchParams();
-        encodedParams.set('source_language', sourceLanguage) // 전달 받은 텍스트 언어
-        encodedParams.set('target_language', targetLanguage)        // 번역할 언어
-        encodedParams.set('text', text)
+    // Rapid API translation
+    // const translationHandler = async (text, sourceLanguage, targetLanguage) => {
+    //     const encodedParams = new URLSearchParams();
+    //     encodedParams.set('source_language', sourceLanguage) // 전달 받은 텍스트 언어
+    //     encodedParams.set('target_language', targetLanguage)        // 번역할 언어
+    //     encodedParams.set('text', text)
 
-        console.log('translationHandler data', encodedParams)
+    //     console.log('translationHandler data', encodedParams)
 
-        const options = {
-            method: 'POST',
-            url: 'https://text-translator2.p.rapidapi.com/translate',
-            headers: {
-                'content-type': 'application/x-www-form-urlencoded',
-                'X-RapidAPI-Key': REACT_APP_X_RAPID_API_KEY,
-                'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
-            },
-            data: encodedParams,
-        };
+    //     const options = {
+    //         method: 'POST',
+    //         url: 'https://text-translator2.p.rapidapi.com/translate',
+    //         headers: {
+    //             'content-type': 'application/x-www-form-urlencoded',
+    //             'X-RapidAPI-Key': REACT_APP_X_RAPID_API_KEY,
+    //             'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
+    //         },
+    //         data: encodedParams,
+    //     };
             
+    //     try {
+    //         const response = await axios.request(options);
+    //         let translatedText = response.data.data.translatedText
+    //         console.log('번역에 성공하였습니다.', translatedText)
+    //         return translatedText
+    //     } catch (error) {
+    //         console.error(error);
+    //         alert('번역에 실패하였습니다.');
+    //     }
+    // };
+
+    // papago API 테스트
+    const translationHandler = async (text, sourceLanguage, targetLanguage) => {
+    
+        const headers = {
+            'Content-Type' : 'application/json',
+            'Authorization': `Bearer ${user.accessToken}`
+        }
+        
+        const requestBody = {
+            text: text,
+            targetLang: targetLanguage,
+            sourceLang: sourceLanguage
+        };
+
+        const requestBodyJSON = JSON.stringify(requestBody);
+
         try {
-            const response = await axios.request(options);
-            let translatedText = response.data.data.translatedText
+            const response = await axios.post(`${BACKEND_URL}/api/v1/naver/translate`, requestBodyJSON, {headers})
+            let translatedText = response.data
             console.log('번역에 성공하였습니다.', translatedText)
             return translatedText
         } catch (error) {
             console.error(error);
-            alert('번역에 실패하였습니다.');
+            // alert('번역에 실패하였습니다.');
+            return text
         }
     };
 
